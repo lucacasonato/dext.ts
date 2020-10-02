1572ef4b92693972958e4e719967671e23e12421
Luca Casonato <lucacasonato@yahoo.com>
2020-10-02 23:32:44 +0200

    fmt

--- deps/test.ts
+++ deps/test.ts
@@ -1,7 +1,7 @@
 export {
   assert,
   assertEquals,
-  assertStringContains
+  assertStringContains,
 } from "https://deno.land/std@0.72.0/testing/asserts.ts";
 export { exists } from "https://deno.land/std@0.72.0/fs/mod.ts";
 import { path } from "./mod.ts";
