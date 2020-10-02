f680de2ae7daeebd1c958a4613f4030c6da7914d
Luca Casonato <lucacasonato@yahoo.com>
2020-10-02 23:34:58 +0200

    sort pages

--- src/util.ts
+++ src/util.ts
@@ -24,6 +24,7 @@ export async function findPages(pagesDir: string): Promise<Pages> {
       pagePaths.push(path.relative(pagesDir, file.path));
     }
   }
+  pagePaths.sort();
   const allPages = await Promise.all(pagePaths.map(async (page) => {
     const name = page.substring(0, page.length - path.extname(page).length);
     const parts = name.split(path.SEP_PATTERN);
