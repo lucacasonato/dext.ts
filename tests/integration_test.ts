60e134b5a87e6ec90d8ae919ffa3f93a28c3cfa7
Bartek Iwańczuk <biwanczuk@gmail.com>
2020-10-04 12:50:27 +0200

    add test

--- tests/integration_test.ts
+++ tests/integration_test.ts
@@ -93,3 +93,24 @@ integrationTest({
     assertEquals(await Deno.readTextFile(ryanJSONPath), `{"str":"RYAN"}`);
   },
 });
+
+integrationTest({
+  name: "create",
+  cmd: ["create"],
+  clean: true,
+  async after(ctx) {
+    assert(ctx.status.success);
+
+    const expectedFiles = [
+      "deps.ts",
+      "tsconfig.json",
+      "pages/index.tsx",
+      "pages/user/[name].tsx",
+    ];
+
+    for (const expectedFile of expectedFiles) {
+      const p = join(ctx.dir, expectedFile);
+      assert(await exists(p));
+    }
+  },
+});
