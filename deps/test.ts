f1b132d28d9f05b4a26be5676c0525819e31ba96
Luca Casonato <lucacasonato@yahoo.com>
2020-10-06 13:36:19 +0200

    chore: update dependencies

--- deps/test.ts
+++ deps/test.ts
@@ -2,11 +2,11 @@ export {
   assert,
   assertEquals,
   assertStringContains,
-} from "https://deno.land/std@0.72.0/testing/asserts.ts";
+} from "https://deno.land/std@0.73.0/testing/asserts.ts";
 export {
   copy,
   ensureDir,
   exists,
-} from "https://deno.land/std@0.72.0/fs/mod.ts";
+} from "https://deno.land/std@0.73.0/fs/mod.ts";
 import { path } from "./mod.ts";
 export const join = path.join;
