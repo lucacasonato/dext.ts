d7d094dbf93cbc6de829921b805d71f9fa8dea95
Luca Casonato <lucacasonato@yahoo.com>
2021-02-03 15:06:05 +0100

    chore: update to preact@10.5.12

--- deps/preact/debug.ts
+++ deps/preact/debug.ts
@@ -1,1 +1,1 @@
-export * from "https://x.lcas.dev/preact@10.5.11/debug.js";
+export * from "https://x.lcas.dev/preact@10.5.12/debug.js";
--- deps/preact/hooks.ts
+++ deps/preact/hooks.ts
@@ -1,1 +1,1 @@
-export * from "https://x.lcas.dev/preact@10.5.11/hooks.js";
+export * from "https://x.lcas.dev/preact@10.5.12/hooks.js";
